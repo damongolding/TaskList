49b2f540d65715019d1326f94ffd69dd5ad6f517
Damon <damon@damongolding.com>
2015-04-19 13:43:48 +0100

    Adding some commenting
    Just to make the code a little clearer

--- TaskList/TaskListController.swift
+++ TaskList/TaskListController.swift
@@ -17,6 +17,7 @@ class TaskListController: UITableViewController, updateData {
     var tasks : Tasks
     
     required init!(coder aDecoder: NSCoder!) {
+        // We will be grabbing our data from CoreData here (at some point)
         self.tasks = Tasks(tasks: ["Get eggs"])
         
         super.init(coder: aDecoder)
@@ -25,10 +26,7 @@ class TaskListController: UITableViewController, updateData {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // Display an Edit button in the navigation bar for this view controller.
         self.navigationItem.leftBarButtonItem = self.editButtonItem()
     }
 
@@ -42,30 +40,48 @@ class TaskListController: UITableViewController, updateData {
         return self.tasks.taskList.count
     }
     
+    // Configure each cell using task info
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
+        // grab our prototype cell using its name "task"
         let cell = tableView.dequeueReusableCellWithIdentifier("task", forIndexPath: indexPath) as! TaskListCell
         
+        // Set title using task data(title)
         cell.configureCell(self.tasks.taskList[indexPath.row])
         
+        // return configured cell to view
         return cell
     }
 
-
+    // set delegate so we can send info/data back
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         var AddNewTask : NewTaskController = NewTaskController()
         AddNewTask = segue.destinationViewController as! NewTaskController
         AddNewTask.delegate = self
     }
     
+    
+    // called via delegate
     func addTask(task: String) {
         
-        self.tasks.addTask(task)
+        // add task to task list
+        self.tasks.addTask(task) // add task to task list
         
+        // putdate codedata here?
+        
+        
+        // Reload view
         dispatch_async(dispatch_get_main_queue(), { () -> Void in
             self.tableView.reloadData() // reload view
         })
     }
+    
+    // func were we will save data to CoreData
+    func saveData(){
+    }
+    
+    // where we will grab data(tasks) from CoreData
+    func getData() -> [String] {return [""]}
     
     
     
