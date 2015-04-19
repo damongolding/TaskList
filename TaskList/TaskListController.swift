a4b8ba2e71e151be4267fa287ea4be2d65edd207
Damon <damon@damongolding.com>
2015-04-20 00:54:41 +0100

    implementing cordite funcs

--- TaskList/TaskListController.swift
+++ TaskList/TaskListController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 protocol updateData{
     func addTask(task:String)
@@ -15,16 +16,22 @@ protocol updateData{
 class TaskListController: UITableViewController, updateData {
     
     var tasks : Tasks
+    var items : [NSManagedObject]?
     
     required init!(coder aDecoder: NSCoder!) {
         // We will be grabbing our data from CoreData here (at some point)
         self.tasks = Tasks(tasks: ["Get eggs"])
         
+        // call super init
         super.init(coder: aDecoder)
+
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // grab data deom CoreData
+        getData()
         
         // Display an Edit button in the navigation bar for this view controller.
         self.navigationItem.leftBarButtonItem = self.editButtonItem()
@@ -67,8 +74,9 @@ class TaskListController: UITableViewController, updateData {
         // add task to task list
         self.tasks.addTask(task) // add task to task list
         
-        // putdate codedata here?
-        
+        // insert into codedata here?
+        saveData(task)
+
         
         // Reload view
         dispatch_async(dispatch_get_main_queue(), { () -> Void in
@@ -77,11 +85,50 @@ class TaskListController: UITableViewController, updateData {
     }
     
     // func were we will save data to CoreData
-    func saveData(){
+    func saveData(title:String){
+        // Retrieve the managed object context in the app delegate
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedContext = appDelegate.managedObjectContext!
+        
+        // Add an item to the managed object context
+        let entity = NSEntityDescription.entityForName("Task", inManagedObjectContext: managedContext)
+        let item = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
+        
+        // set the value for the item
+        item.setValue(title, forKey: "taskTitle")
+        
+        // Save the managed object context back
+        var error: NSError?
+        if !managedContext.save(&error) {
+            println("Could not save \(error), \(error?.userInfo)")
+        }
+        
+        // Add the new item to local items data source
+        self.items!.append(item)
+        println(self.items)
     }
     
     // where we will grab data(tasks) from CoreData
-    func getData() -> [String] {return [""]}
+    func getData() {
+        
+        // Get the managed object context
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedObjectContext = appDelegate.managedObjectContext!
+        
+        // Create a fetch request into Core Data
+        let fetchRequest = NSFetchRequest(entityName: "Task")
+        
+        // Execute the fetch request
+        var error: NSError?
+        
+        if let fetchedResults = managedObjectContext.executeFetchRequest(fetchRequest, error: &error) as! [NSManagedObject]?
+        {
+            // fetchedResults is not nil. update self.items
+            self.items = fetchedResults
+        } else {
+            println("Could not fetch \(error), \(error!.userInfo)")
+        }
+    }
     
     
     
